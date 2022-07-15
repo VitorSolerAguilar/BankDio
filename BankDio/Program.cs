785c82442e9ee71174f938cd251b668a771d26b1
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2022-07-14 23:53:18 -0300

    Layout da tela principal

--- BankDio/Program.cs
+++ BankDio/Program.cs
@@ -6,7 +6,54 @@ namespace BankDio
     {
         static void Main(string[] args)
         {
-            
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ListarContas();
+                        break;
+                    case "2":
+                        //InserirConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                        //Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                }
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine(" ========================");
+            Console.WriteLine(" Dio Bank a seu dispor!");
+            Console.WriteLine(" ========================= \r\n");
+
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Listar Contas");
+            Console.WriteLine("2 - Inserir Nova Conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair \r\n");
+
+            string OpcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return OpcaoUsuario;
         }
     }
 }
