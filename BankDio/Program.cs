57a671e28094379248c741cd79001ee524632c61
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2022-07-14 21:55:34 -0300

    Projeto Criado

---
+++ BankDio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankDio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
