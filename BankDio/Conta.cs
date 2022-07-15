db1f67afde644de8dc90c583b501fb832d75232c
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2022-07-14 22:07:38 -0300

    Adicionadas propriedades base

---
+++ BankDio/Conta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDio
+{
+    class Conta
+    {
+        public Conta(TipoContaEnum tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public TipoContaEnum TipoConta{ get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+    }
+}
